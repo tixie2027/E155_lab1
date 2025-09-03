077cba65724b7c9dadd32492c4e9a5802fb540c6
Tian Xie <145227971+tixie2027@users.noreply.github.com>
2025-09-03 16:34:58 -0700

    Refactor top module for LED and segment control
    Refactor top module to include 4-bit input and 3-bit LED output. Added logic for LED control and integrated 7-segment display module.

--- fpga/src/top.sv
+++ fpga/src/top.sv
@@ -1,23 +1,41 @@
-module top(
-	input 	logic	mcu_blink_in,
-	output 	logic fpga_blink_out, mcu_echo_led
-);
+// Tian Xie
+// tixie@hmc.edu
+// 9/3/2025
+// This is the top module. It includes the 3 blinking LED control code, a HSOSC, and also calls the 7 segment module
 
-	logic int_osc;
-	logic pulse;
+module top(
+    input logic [3:0] s,
+    output logic [2:0] led,
+    output logic [6:0] seg);
+
+    assign led[0] = s[1] ^ s[0];
+	assign led[1] = s[2] & s[3];
+
+    logic int_osc;
 	logic led_state = 0;
 	logic [24:0] counter = 0;
 	
-	// Internal high-speed oscillator
 	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
-	
-	// Simple clock divider
-	always_ff @(posedge int_osc)
+
+    always_ff @(posedge int_osc)
 		begin
-			counter <= counter + 1;
-		end
-		
-  assign fpga_blink_out = counter[24];
-	assign mcu_echo_led = mcu_blink_in;
+            if (counter == 25'd10000000) begin
+                counter <= 0;
+                led_state <= ~led_state;
+            end else begin 
+			    counter <= counter + 1;
+		    end
+        end
+        
+    assign led[2] = led_state;
+	
+	// calls 7 segments
+    segment7 s7 (s, seg);
 
 endmodule
+
+
+
+
+
+
